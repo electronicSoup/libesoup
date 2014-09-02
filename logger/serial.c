d8fc8db729678450c6147137c4a59730d7253556
John Whitmore <johnfwhitmore@gmail.com>
2014-09-02 19:33:37 +0100

    Changed the way the serial port is defined to better represent a serial port corrected indentation.
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>

--- logger/serial.c
+++ logger/serial.c
@@ -26,29 +26,37 @@
 #if (LOG_LEVEL < NO_LOGGING)
 void serial_init(void)
 {
-    /*
-     * Serial Outout pin should be defined in include file system.h
-     */
-#if SERIAL_PORT == 20
-    RPOR10bits.RP20R = 3;
-#elif SERIAL_PORT == 6
-    RPOR3bits.RP6R = 3;
-#elif SERIAL_PORT == 26
-    RPOR13bits.RP26R = 3;
+	/*
+	 * Serial Outout pin should be defined in include file system.h
+	 */
+#if defined(SERIAL_PORT_GndTxRx)
+	RPOR12bits.RP25R = 3;
+#elif defined(SERIAL_PORT_GndRxTx)
+	RPOR10bits.RP20R = 3;
 #endif
 
-    U1MODE = 0x8800;
-    U1STA = 0x0410;
+#if 0
+#if SERIAL_PORT == 20  // <-
+	RPOR10bits.RP20R = 3;
+#elif SERIAL_PORT == 6
+	RPOR3bits.RP6R = 3;
+#elif SERIAL_PORT == 26
+	RPOR13bits.RP26R = 3;
+#endif
+#endif //0
 
-    /*
-     * Desired Baud Rate = FCY/(16 (UxBRG + 1))
-     *
-     * UxBRG = ((FCY/Desired Baud Rate)/16) - 1
-     *
-     * UxBRG = ((CLOCK/SERIAL_BAUD)/16) -1
-     *
-     */
-    U1BRG = ((CLOCK_FREQ/SERIAL_BAUD)/16) -1;
+	U1MODE = 0x8800;
+	U1STA = 0x0410;
+
+	/*
+	 * Desired Baud Rate = FCY/(16 (UxBRG + 1))
+	 *
+	 * UxBRG = ((FCY/Desired Baud Rate)/16) - 1
+	 *
+	 * UxBRG = ((CLOCK/SERIAL_BAUD)/16) -1
+	 *
+	 */
+	U1BRG = ((CLOCK_FREQ / SERIAL_BAUD) / 16) - 1;
 }
 #endif // (LOG_LEVEL < NO_LOGGING)
 
@@ -60,37 +68,35 @@ void serial_log(log_level_t level, char* tag, char* fmt, ...)
 void serial_log(log_level_t level, const rom char* tag, const rom char* fmt, ...)
 #endif
 {
-    va_list args;
+	va_list args;
 
-    switch (level)
-    {
-        case Debug:
-            printf(" D :");
-            break;
+	switch (level) {
+		case Debug:
+			printf(" D :");
+			break;
 
-        case Info:
-            printf(" I :");
-            break;
+		case Info:
+			printf(" I :");
+			break;
 
-        case Warning:
-            printf(" W :");
-            break;
+		case Warning:
+			printf(" W :");
+			break;
 
-        case Error:
-            printf("***\n\r");
-            printf("*E*:");
-            break;
-    }
-    printf(tag);
-    printf(":");
+		case Error:
+			printf("***\n\r");
+			printf("*E*:");
+			break;
+	}
+	printf(tag);
+	printf(":");
 
-    va_start(args, fmt);
-    vprintf(fmt, args);
-    va_end(args);
+	va_start(args, fmt);
+	vprintf(fmt, args);
+	va_end(args);
 
-    if(level == Error)
-    {
-        printf("***\n\r");
-    }
+	if (level == Error) {
+		printf("***\n\r");
+	}
 }
 #endif //(LOG_LEVEL < NO_LOGGING)
