e49cbabb6091699edd15a8ce4c01f8c1600618b5
John Whitmore <johnfwhitmore@gmail.com>
2014-09-04 18:08:13 +0100

    re-ordered the defines in example system.h
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>

--- example_system.h
+++ example_system.h
@@ -7,13 +7,13 @@
 #define SERIAL_BAUD 19200
 
 /*
- * Log level. these are defined in es_lib/core.h as they are included everywhere
- */
-#define LOG_LEVEL LOG_DEBUG
-
-/*
  * The configuration of the Serial Port there are 3 pins, the Gnd pin is fixed
  * so you define the configuration of the other two pins. Uncomment as required
  */
 //#define SERIAL_PORT_GndTxRx //== 20
 #define SERIAL_PORT_GndRxTx //== 20
+
+/*
+ * Log level. these are defined in es_lib/core.h as they are included everywhere
+ */
+#define LOG_LEVEL LOG_DEBUG
