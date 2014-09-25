0b0e5e2e8d731cdbd9f4bd1aef3c8e5366d9925c
John Whitmore <johnfwhitmore@gmail.com>
2014-09-25 10:34:03 +0100

    Added the number of system timers to the example system.h file.
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>

--- example_system.h
+++ example_system.h
@@ -17,3 +17,12 @@
  * Log level. these are defined in es_lib/core.h as they are included everywhere
  */
 #define LOG_LEVEL LOG_DEBUG
+
+/*
+ * NUMBER_OF_TIMERS
+ * 
+ * Definition of the number of system timers required in the system. Increasing 
+ * the number of timers uses more system RAM. This will depend on the estimated
+ * demand for timers in your design.
+ */
+#define NUMBER_OF_TIMERS 20
