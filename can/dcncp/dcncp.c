d6e47d3c693a6641ace39341228826f23de9fada
John Whitmore <johnfwhitmore@gmail.com>
2014-12-15 22:46:10 +0000

    code cleanup
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>

--- can/dcncp/dcncp.c
+++ can/dcncp/dcncp.c
@@ -1,6 +1,6 @@
 /**
  *
- * \file es_lib/dcncp/dcncp.h
+ * \file es_lib/can/dcncp/dcncp.c
  *
  * Dynamic CAN Node Configuration Protocol 
  *
@@ -35,14 +35,14 @@
 #include "serial.h"
 #endif
 
-#include "es_lib/dcncp/dcncp.h"
+#include "es_lib/can/dcncp/dcncp.h"
 #include "es_lib/timers/timers.h"
 #include "es_lib/can/es_can.h"
 #if defined(CAN_LAYER_3)
 #include "es_lib/logger/net.h"
 #endif
 
-#define TAG "NODE_ADDRESS"
+#define TAG "CAN_DCNCP"
 
 #define NodeInfo 0x01;
 
@@ -56,6 +56,7 @@ static es_timer nodeRegisteredTimer;
 
 //static bool this_node_net_logger = FALSE;
 
+#if defined(CAN_LAYER_3)
 static can_frame local_net_logger_frame;
 static es_timer local_net_logger_timer;
 
@@ -63,10 +64,10 @@ static es_timer local_net_logger_timer;
 
 static void exp_net_logger_ping(timer_t timer_id __attribute__((unused)), union sigval);
 
-
 static void exp_sendAddressRegisterReq(timer_t timer_id, union sigval data);
 static void exp_nodeAddressRegistered(timer_t timer_id, union sigval data);
 static void exp_sendNodeAddressReport(timer_t timer_id, union sigval data);
+#endif
 
 static void l2MsgHandler(can_frame *msg);
 
@@ -80,27 +81,29 @@ static void (*status_handler)(u8 mask, can_status_t status, baud_rate_t baud) = 
 
 void dcncp_init(void (*arg_status_handler)(u8 mask, can_status_t status, baud_rate_t baud))
 {
-	can_target_t target;
+	can_l2_target_t target;
+#if defined(CAN_LAYER_3)
 	result_t result;
+#endif
 
         status_handler = arg_status_handler;
         status.byte = 0x00;
 
 #if defined(CAN_LAYER_3)
 	TIMER_INIT(sendRegisterReqTimer);
+	TIMER_INIT(local_net_logger_timer);
 #endif
 	TIMER_INIT(nodeRegisteredTimer);
-	TIMER_INIT(local_net_logger_timer);
 
 	/*
 	 * Add the Layer 2 and Layer 3 Can Message Handlers
 	 */
-	target.mask = (u32)NODE_MASK;
-	target.filter = (u32)NODE_FILTER;
+	target.mask = (u32)CAN_DCNCP_MASK;
+	target.filter = (u32)CAN_DCNCP_FILTER;
 	target.handler = l2MsgHandler;
 
 	LOG_D("Node Address Register handler Mask 0x%x, Filter 0x%x\n\r", target.mask, target.filter);
-	l2_reg_handler(&target);
+	can_l2_reg_handler(&target);
 
 #if defined(CAN_LAYER_3)
 	/*
@@ -135,20 +138,20 @@ void exp_sendAddressRegisterReq(timer_t timer_id __attribute__((unused)), union 
 
 	get_l3_node_address(&address);
 
-		LOG_D("sendRegisterReq(%x)\n\r", (u16)address);
+	LOG_D("sendRegisterReq(%x)\n\r", (u16) address);
 
-		msg.can_id = AddressRegisterReq;
-		msg.can_dlc = 1;
-		msg.data[0] = address;
+	msg.can_id = CAN_DCNCP_AddressRegisterReq;
+	msg.can_dlc = 1;
+	msg.data[0] = address;
 
-		l2_tx_frame(&msg);
+	can_l2_tx_frame(&msg);
 
-		// Create a 2 Second timer if no reject is recieved in that time
-		// this node shall consider itself registered
-		result = timer_start(SECONDS_TO_TICKS(2), exp_nodeAddressRegistered, (union sigval)(void *)NULL, &nodeRegisteredTimer);
-		if(result != SUCCESS) {
-			LOG_E("Failed to start Node Registered Timer\n\r");
-		}
+	// Create a 2 Second timer if no reject is recieved in that time
+	// this node shall consider itself registered
+	result = timer_start(SECONDS_TO_TICKS(2), exp_nodeAddressRegistered, (union sigval)(void *) NULL, &nodeRegisteredTimer);
+	if (result != SUCCESS) {
+		LOG_E("Failed to start Node Registered Timer\n\r");
+	}
 }
 #endif
 
@@ -182,24 +185,25 @@ void exp_nodeAddressRegistered(timer_t timer_id __attribute__((unused)), union s
 
 void l2MsgHandler(can_frame *msg)
 {
+#if defined(CAN_LAYER_3)
 	u8 address;
 	can_frame txMsg;
 	es_timer timer;
 	result_t result;
-
+#endif
 	LOG_D("Node Adress message received 0x%lx\n\r", msg->can_id);
-	if (msg->can_id == AddressRegisterReq) {
+	if (msg->can_id == CAN_DCNCP_AddressRegisterReq) {
 #if defined(CAN_LAYER_3)
 		get_l3_node_address(&address);
 
 		if(msg->data[0] == address) {
 			if(status.bit_field.dcncp_status & DCNCP_L3_Address_Finalised) {
 				LOG_D("reject Register Request\n\r");
-				txMsg.can_id = AddressRegisterReject;
+				txMsg.can_id = CAN_DCNCP_AddressRegisterReject;
 				txMsg.can_dlc = 1;
 				txMsg.data[0] = address;
 
-				l2_tx_frame(&txMsg);
+				can_l2_tx_frame(&txMsg);
 			} else {
 				LOG_D("Register Node Address clash\n\r");
 				//Have to create a new node address for this node
@@ -212,18 +216,18 @@ void l2MsgHandler(can_frame *msg)
 			}
 		}
 #endif
-	} else if(msg->can_id == AddressRegisterReject) {
+	} else if(msg->can_id == CAN_DCNCP_AddressRegisterReject) {
 #if defined(CAN_LAYER_3)
 		get_l3_node_address(&address);
 
 		if(msg->data[0] == address) {
 			if(status.bit_field.dcncp_status & DCNCP_L3_Address_Finalised) {
 				LOG_E("Sending Can Error Message\n\r");
-				txMsg.can_id = NodeAddressError;
+				txMsg.can_id = CAN_DCNCP_NodeAddressError;
 				txMsg.can_dlc = 1;
 				txMsg.data[0] = address;
 
-				l2_tx_frame(&txMsg);
+				can_l2_tx_frame(&txMsg);
 			} else {
 				//Have to create a new node address for this node
 				//cancel the timers
@@ -235,7 +239,7 @@ void l2MsgHandler(can_frame *msg)
 			}
 		}
 #endif
-	} else if (msg->can_id == NodeAddressReportReq) {
+	} else if (msg->can_id == CAN_DCNCP_NodeAddressReportReq) {
 #if defined(CAN_LAYER_3)
 		// Create a random timer between 100 and  1000 miliSeconds for firing node report message
 		result = timer_start(MILLI_SECONDS_TO_TICKS((u16) ((rand() % 900) + 100)), exp_sendNodeAddressReport, (union sigval)(void *)NULL, &timer);
@@ -243,27 +247,27 @@ void l2MsgHandler(can_frame *msg)
 			LOG_E("Failed to start Node Registered Timer\n\r");
 		}
 #endif
-	} else if (msg->can_id == NodeAddressReporting) {
+	} else if (msg->can_id == CAN_DCNCP_NodeAddressReporting) {
 		if(msg->data[0]) {
 			LOG_D("Foreign Node Rep Registered Node Address 0x%x\n\r", msg->data[1]);
 		} else {
 			LOG_D("Foreign Node Rep UN-Registered Node Address 0x%x\n\r", msg->data[1]);
 		}
-	} else if (msg->can_id == NodeSetBaudRate) {
+	} else if (msg->can_id == CAN_DCNCP_NodeSetBaudRate) {
 		LOG_D(Debug, TAG, "***Baud Rate Change Request New Baud Rate %s\n\r", baud_rate_strings[msg->data[0]]);
 //TODO		L2_SetCanNodeBuadRate(msg->data[0]);
-	} else if (msg->can_id == NodePingMessage) {
+	} else if (msg->can_id == CAN_DCNCP_NodePingMessage) {
 #if DEBUG_LEVEL <= LOG_DEBUG
 		printf(".");
 #endif
-	} else if (msg->can_id == NetLogger) {
+	} else if (msg->can_id == CAN_DCNCP_NetLogger) {
 		LOG_D("Received NetLogger Message\n\r");
 #if defined(CAN_LAYER_3)
 		net_logger_foreign_register(msg->data[0], msg->data[1]);
 #else
 		LOG_D("Ignoring NetLogger Message NO LAYER 3!\n\r");
 #endif
-	} else if (msg->can_id == CancelNetLogger) {
+	} else if (msg->can_id == CAN_DCNCP_CancelNetLogger) {
 		LOG_D("Received CancelNetLogger Message\n\r");
 #if defined(CAN_LAYER_3)
 		net_logger_foreign_cancel(msg->data[0]);
@@ -290,7 +294,7 @@ void exp_sendNodeAddressReport(timer_t timer_id __attribute__((unused)), union s
 
 	LOG_D("exp_sendNodeAddressReport(Address %x)\n\r", address);
 
-	txMsg.can_id = NodeAddressReporting;
+	txMsg.can_id = CAN_DCNCP_NodeAddressReporting;
 	txMsg.can_dlc = 2;
 	txMsg.data[1] = address;
 
@@ -299,7 +303,7 @@ void exp_sendNodeAddressReport(timer_t timer_id __attribute__((unused)), union s
 	else
 		txMsg.data[0] = FALSE;
 
-	l2_tx_frame(&txMsg);
+	can_l2_tx_frame(&txMsg);
 }
 #endif
 
@@ -324,7 +328,7 @@ result_t register_this_node_net_logger(log_level_t level)
 		local_net_logger_frame.data[0] = address;
 		local_net_logger_frame.data[1] = level;
 
-		l2_tx_frame(&local_net_logger_frame);
+		can_l2_tx_frame(&local_net_logger_frame);
 		LOG_D("NetLogger message sent\n\r");
 		timer_start(LOCAL_NET_LOGGER_MSG_PERIOD, exp_net_logger_ping, (union sigval)(void *)NULL, &local_net_logger_timer);
 
@@ -341,7 +345,7 @@ void exp_net_logger_ping(timer_t timer_id __attribute__((unused)), union sigval 
 {
 	data = data;
 
-	l2_tx_frame(&local_net_logger_frame);
+	can_l2_tx_frame(&local_net_logger_frame);
 	LOG_D("NetLogger message sent\n\r");
 	timer_start(LOCAL_NET_LOGGER_MSG_PERIOD, exp_net_logger_ping, (union sigval)(void *)NULL, &local_net_logger_timer);
 }
@@ -356,11 +360,11 @@ result_t unregister_this_node_net_logger()
 	LOG_D("DeRegAsNetLogger()\n\r");
 	get_l3_node_address(&address);
 
-	txMsg.can_id = CancelNetLogger;
+	txMsg.can_id = CAN_DCNCP_CancelNetLogger;
 	txMsg.can_dlc = 1;
 	txMsg.data[0] = address;
 
-	l2_tx_frame(&txMsg);
+	can_l2_tx_frame(&txMsg);
 	LOG_D("CancelNetLogger message sent\n\r");
 
 	if(local_net_logger_timer.status == ACTIVE)
@@ -374,10 +378,10 @@ void send_ping_message(void)
 {
 	can_frame txMsg;
 
-	txMsg.can_id = NodePingMessage;
+	txMsg.can_id = CAN_DCNCP_NodePingMessage;
 	txMsg.can_dlc = 0;
 
-	l2_tx_frame(&txMsg);
+	can_l2_tx_frame(&txMsg);
 	LOG_D("Ping message sent\n\r");
 }
 
