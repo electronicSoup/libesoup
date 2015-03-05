ee3f290fb0afb4bf55d9dd105bcc6e7c4157b372
John Whitmore <johnfwhitmore@gmail.com>
2015-03-05 23:25:44 +0000

    changes for the App's interface to the OS
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>

--- app/app.h
+++ app/app.h
@@ -19,9 +19,7 @@
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  *
  */
-#include <p24Fxxxx.h>
-_CONFIG1(JTAGEN_OFF & FWDTEN_ON & FWPSA_PR32 & WDTPS_PS1024 & WINDIS_OFF & ICS_PGx2)   // JTAG off, watchdog timer on
-_CONFIG2(FNOSC_FRCPLL & POSCMOD_NONE & OSCIOFNC_ON & PLL_96MHZ_ON & PLLDIV_NODIV & DISUVREG_OFF)  // CLOCK 16000000
+#include "es_lib/core.h"
 
 /*
  * The following are definitions for setting up the identification strings
@@ -35,17 +33,11 @@ DEF_APP_VERSION_10("v1.0")
 DEF_APP_URL_50("www.test.com")
 
  */
-//#define APP_STRINGS_BASE           0x18000
-#define APP_STRINGS_BASE           0x400
+#define APP_STRINGS_BASE           0x18000
 #define APP_AUTHOR_40_ADDR         APP_STRINGS_BASE
 #define APP_DESCRIPTION_50_ADDR    APP_AUTHOR_40_ADDR + 40
 #define APP_VERSION_10_ADDR        APP_DESCRIPTION_50_ADDR + 50
 #define APP_URL_50_ADDR            APP_VERSION_10_ADDR + 10
-
-//__prog__ char app_author[40] __attribute__((space(prog), address(APP_STRINGS_BASE))) = "me@bogus.com";
-//__prog__ char app_description[50] __attribute__((space(prog), address(APP_STRINGS_BASE + 40))) = "My First App";
-//__prog__ char app_version[10] __attribute__((space(prog), address(APP_STRINGS_BASE + 40 + 50))) = "1.0";
-//__prog__ char app_uri[50] __attribute__((space(prog), address(APP_STRINGS_BASE + 40 + 50 + 10))) = "http:/www.bogus.com";
 
 #define DEF_APP_AUTHOR_40(x)      __prog__ char app_author[40]       __attribute__ ((space(prog),address(APP_AUTHOR_40_ADDR))) = x;
 #define DEF_APP_DESCRIPTION_50(x) __prog__ char app_description[50]  __attribute__ ((space(prog),address(APP_DESCRIPTION_50_ADDR))) = x;
