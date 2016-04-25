3bfe6ffdcd31d7a105a1296a88e1dd40a5051b04
John Whitmore <johnfwhitmore@gmail.com>
2016-04-25 12:39:18 +0100

    Added the Structure needed for timer data
    Changed ISR's to take "repeat" into account
    added prototype for start_timer function which will do the work.
    
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>

--- timers/hw_timers.c
+++ timers/hw_timers.c
@@ -28,17 +28,29 @@
 #include "es_lib/timers/hw_timers.h"
 #include "es_lib/logger/serial_log.h"
 
+struct hw_timer_data {
+	u8 active:1;
+	u8 repeat:1;
+	void (*expiry_function)(void);
+};
+
+static struct hw_timer_data timers[NUMBER_HW_TIMERS];
+
 static void hw_timer_isr(u8 timer);
+static u8   start_timer(u8 timer, ty_time_units units, u16 time, u8 repeat, void (*expiry_function)(void));
 
 void _ISR __attribute__((__no_auto_psv__)) _T1Interrupt(void)
 {
 	IFS0bits.T1IF = 0;
+	TMR1 = 0x00;
 
-	/*
-	 * Timer is off for the moment.
-	 */
-	IEC0bits.T1IE = 0;
-	T1CONbits.TON = 0;
+	if(!timers[TIMER_1 - TIMER_1].repeat) {
+		/*
+		 * Timer is off for the moment.
+		 */
+		IEC0bits.T1IE = 0;
+		T1CONbits.TON = 0;
+	}
 
 	hw_timer_isr(TIMER_1);
 }
@@ -46,12 +58,15 @@ void _ISR __attribute__((__no_auto_psv__)) _T1Interrupt(void)
 void _ISR __attribute__((__no_auto_psv__)) _T2Interrupt(void)
 {
 	IFS0bits.T2IF = 0;
+	TMR2 = 0x00;
 
-	/*
-	 * Timer is off for the moment.
-	 */
-	IEC0bits.T2IE = 0;
-	T2CONbits.TON = 0;
+	if(!timers[TIMER_2 - TIMER_1].repeat) {
+		/*
+		 * Timer is off for the moment.
+		 */
+		IEC0bits.T2IE = 0;
+		T2CONbits.TON = 0;
+	}
 
 	hw_timer_isr(TIMER_2);
 }
@@ -59,39 +74,45 @@ void _ISR __attribute__((__no_auto_psv__)) _T2Interrupt(void)
 void _ISR __attribute__((__no_auto_psv__)) _T3Interrupt(void)
 {
 	IFS0bits.T3IF = 0;
+	TMR3 = 0x00;
 
-	/*
-	 * Timer is off for the moment.
-	 */
-	IEC0bits.T3IE = 0;
-	T3CONbits.TON = 0;
-
+	if(!timers[TIMER_3 - TIMER_1].repeat) {
+		/*
+		 * Timer is off for the moment.
+		 */
+		IEC0bits.T3IE = 0;
+		T3CONbits.TON = 0;
+	}
 	hw_timer_isr(TIMER_3);
 }
 
 void _ISR __attribute__((__no_auto_psv__)) _T4Interrupt(void)
 {
 	IFS1bits.T4IF = 0;
+	TMR4 = 0x00;
 
-	/*
-	 * Timer is off for the moment.
-	 */
-	IEC1bits.T4IE = 0;
-	T4CONbits.TON = 0;
-
+	if(!timers[TIMER_4 - TIMER_1].repeat) {
+		/*
+		 * Timer is off for the moment.
+		 */
+		IEC1bits.T4IE = 0;
+		T4CONbits.TON = 0;
+	}
 	hw_timer_isr(TIMER_4);
 }
 
 void _ISR __attribute__((__no_auto_psv__)) _T5Interrupt(void)
 {
 	IFS1bits.T5IF = 0;
+	TMR5 = 0x00;
 
-	/*
-	 * Timer is off for the moment.
-	 */
-	IEC1bits.T5IE = 0;
-	T5CONbits.TON = 0;
-
+	if(!timers[TIMER_5 - TIMER_1].repeat) {
+		/*
+		 * Timer is off for the moment.
+		 */
+		IEC1bits.T5IE = 0;
+		T5CONbits.TON = 0;
+	}
 	hw_timer_isr(TIMER_5);
 }
 
@@ -100,13 +121,46 @@ static void hw_timer_isr(u8 timer)
 
 }
 
+void hw_timer_init(void)
+{
+	u8 loop;
+
+	LOG_D("hw_timer_init()\n\r");
+
+	for(loop = 0; loop < NUMBER_HW_TIMERS; loop++) {
+		timers[loop].active = 0;
+		timers[loop].repeat = 0;
+		timers[loop].expiry_function = (void (*)(void))NULL;
+	}
+}
+
 u8 hw_timer_start(ty_time_units units, u16 time, u8 repeat, void (*expiry_function)(void))
 {
+	u8 loop;
+	u8 started = FALSE;
+
 	LOG_D("start_hw_timer()\n\r");
+
+	/*
+	 * Find a free timer
+	 */
+	loop = 0;
+
+	while((loop < NUMBER_HW_TIMERS) && !started) {
+		if(!timers[loop].active) {
+			started = start_timer(loop, units, time, repeat, expiry_function);
+		}
+		loop++;
+	}
 }
 
 
 void hw_timer_cancel(u8 timer)
+{
+
+}
+
+static u8   start_timer(u8 timer, ty_time_units units, u16 time, u8 repeat, void (*expiry_function)(void))
 {
 
 }
--- timers/hw_timers.h
+++ timers/hw_timers.h
@@ -1,9 +1,10 @@
 
-#define TIMER_1 1
-#define TIMER_2 2
-#define TIMER_3 3
-#define TIMER_4 4
-#define TIMER_5 5
+#define NUMBER_HW_TIMERS  5
+#define TIMER_1 0
+#define TIMER_2 1
+#define TIMER_3 2
+#define TIMER_4 3
+#define TIMER_5 4
 
 typedef enum {
     uSeconds,
@@ -11,6 +12,6 @@ typedef enum {
     Seconds
 } ty_time_units;
 
-
+extern void hw_timer_init(void);
 extern u8 hw_timer_start(ty_time_units units, u16 time, u8 repeat, void (*expiry_function)(void));
 extern void hw_timer_cancel(u8 timer);
