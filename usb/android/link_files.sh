af95323e8b72416a9547a5745d0f65dc37e94991
John Whitmore <johnfwhitmore@gmail.com>
2016-04-30 15:59:42 +0100

    Updated the bash script for linking to microchip's USB Files
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>

--- usb/android/link_files.sh
+++ usb/android/link_files.sh
@@ -4,9 +4,13 @@
 # 
 # in src directory
 #
-ln -s /opt/microchip/mla/v2014_07_22/framework/usb/src/usb_hal_local.h
-ln -s /opt/microchip/mla/v2014_07_22/framework/usb/src/usb_host_android.c
-ln -s /opt/microchip/mla/v2014_07_22/framework/usb/src/usb_host.c
-ln -s /opt/microchip/mla/v2014_07_22/framework/usb/src/usb_host_local.h
+# ln -s /opt/microchip/mla/v2014_07_22/framework/usb/src/usb_hal_local.h
+# ln -s /opt/microchip/mla/v2014_07_22/framework/usb/src/usb_host_android.c
+# ln -s /opt/microchip/mla/v2014_07_22/framework/usb/src/usb_host.c
+# ln -s /opt/microchip/mla/v2014_07_22/framework/usb/src/usb_host_local.h
 
+ln -s /opt/microchip/mla/v2015_08_10/framework/usb/src/usb_hal_local.h
+ln -s /opt/microchip/mla/v2015_08_10/framework/usb/src/usb_host_android.c
+ln -s /opt/microchip/mla/v2015_08_10/framework/usb/src/usb_host.c
+ln -s /opt/microchip/mla/v2015_08_10/framework/usb/src/usb_host_local.h
 
