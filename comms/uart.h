0b9ce43dfa461da3ac0fa3fdeef63c6374e11059
John Whitmore <johnfwhitmore@gmail.com>
2016-05-02 21:28:39 +0100

    Cleaned up uart.h and added file header
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>

--- comms/uart.h
+++ comms/uart.h
@@ -1,3 +1,26 @@
+/**
+ *
+ * \file es_lib/comms/uart.h
+ *
+ * UART functionalty for the electronicSoup Cinnamon Bun
+ *
+ * Copyright 2016 John Whitmore <jwhitmore@electronicsoup.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the version 2 of the GNU General Public License
+ * as published by the Free Software Foundation
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ *******************************************************************************
+ *
+ */
 
 typedef struct {
     u8            tx_pin;
@@ -5,20 +28,8 @@ typedef struct {
     u8            uart;
     u16           uart_mode;
     u16           baud;
-//    u8            line_termination;
-//    ty_time_units first_resp_char_units;
-//    u16           time_for_first_resp_char;
-//    ty_time_units last_character_units;
-//    u16           timeout_last_character_ms;
-//    void        (*process_line)(u8 *, u16);
-//    u8           *rx_buffer;
-//    u16           rx_buffer_len;
-//    u16           rx_buffer_size;
     void        (*tx_finished)(void);
     void        (*process_rx_char)(u8);
-//    u8           *tx_buffer;
-//    u16           tx_buffer_read_index;
-//    u16           tx_buffer_size;
 } uart_data;
 
 
