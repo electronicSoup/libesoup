487558d44139498c1dad7f7f7f40e7e617655f3c
John Whitmore <johnfwhitmore@gmail.com>
2017-03-04 00:20:19 +0000

    Include clock.h header file it's required.
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>

--- processors/pic24FJ256GB106.c
+++ processors/pic24FJ256GB106.c
@@ -2,9 +2,7 @@
 
 #include <xc.h>
 #include "es_lib/utils/spi.h"
-
-//_CONFIG1(JTAGEN_OFF & FWDTEN_OFF & FWPSA_PR32 & WDTPS_PS1024 & WINDIS_OFF & ICS_PGx2)   // JTAG off, watchdog timer off
-//_CONFIG2(FNOSC_FRCPLL & POSCMOD_NONE & OSCIOFNC_ON & PLL_96MHZ_ON & PLLDIV_NODIV & DISUVREG_OFF)  // CLOCK 16000000
+#include "es_lib/utils/clock.h"
 
 /*
  * /opt/microchip/mla/v2015_08_10/apps/usb/device/hid_joystick/firmware/src/system_config/exp16/pic24fj256gb110_pim/system.c
