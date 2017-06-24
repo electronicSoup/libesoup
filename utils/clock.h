ebd375890969516d773ea3bfc0d3f2edb9e5f380
John Whitmore <johnfwhitmore@gmail.com>
2017-06-24 23:14:57 +0100

    Added doxygen function comment to initialisation function
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>

--- utils/clock.h
+++ utils/clock.h
@@ -22,6 +22,15 @@
 #ifndef _CLOCK_H
 #define _CLOCK_H
 
+/**
+ * Function to initialise the micro-controller's required clock frequency, which
+ * should be defined, as SYS_CLOCK_FREQ, in your libesoup_config.h file. See
+ * example definition in libesoup/examples/libesoup_config.h
+ * 
+ * The initialisation of the clock signal depends on the Crystal being used by
+ * the hardware. This is defined as CRYSTAL_FREQ in the board header file 
+ * included from libesoup/boards. 
+ */
 extern void clock_init(void);
 
 #endif // _CLOCK_H
