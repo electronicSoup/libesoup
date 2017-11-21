aa64d90d1ec1840dec631578e4f78bc1eafb90f1
John Whitmore <johnfwhitmore@gmail.com>
2017-11-21 17:21:52 +0000

    Corrected serial logging calls

--- timers/sw_timers.c
+++ timers/sw_timers.c
@@ -216,9 +216,11 @@ void timer_tick(void)
 				/*
 				 * timer expired so call expiry function.
 				 */
+#ifdef SYS_SERIAL_LOGGING
 #if (DEBUG_FILE && (SYS_LOG_LEVEL <= LOG_DEBUG))
                                 LOG_D("Expiry\n\r");
 #endif
+#endif // SYS_SERIAL_LOGGING
 				timers[loop].active = FALSE;
 
 				function = timers[loop].function;
@@ -285,9 +287,11 @@ result_t sw_timer_start(timer_id *timer, struct timer_req *request)
 	if(*timer != BAD_TIMER_ID) {
                 if(*timer < SYS_NUMBER_OF_SW_TIMERS) {
                         if (timers[*timer].active) {
+#ifdef SYS_SERIAL_LOGGING
 #if (DEBUG_FILE == TRUE) && (SYS_LOG_LEVEL <= LOG_WARNING)
                                 LOG_W("sw_timer_start() timer passed in active. Cancelling!\n\r");
 #endif
+#endif // SYS_SERIAL_LOGGING
                         }
                 }
 		sw_timer_cancel(*timer);
@@ -306,9 +310,11 @@ result_t sw_timer_start(timer_id *timer, struct timer_req *request)
 	 */
 	for(loop=0; loop < SYS_NUMBER_OF_SW_TIMERS; loop++) {
 		if (!timers[loop].active) {
+#ifdef SYS_SERIAL_LOGGING
 #if (DEBUG_FILE && (SYS_LOG_LEVEL <= LOG_DEBUG))
 //                        LOG_D("Using SW timer %d\n\r", loop);
 #endif
+#endif // SYS_SERIAL_LOGGING
 			/*
 			 * Found an inactive timer so assign to this expiry
 			 */
@@ -329,19 +335,22 @@ result_t sw_timer_start(timer_id *timer, struct timer_req *request)
 			 */
 			if(hw_timer_paused) {
 				if(hw_timer_restart(&hw_timer, &hw_timer_req) != SUCCESS) {
+#ifdef SYS_SERIAL_LOGGING
 #if (SYS_LOG_LEVEL <= LOG_ERROR)
 					LOG_E("Failed to restart HW timer\n\r");
 #endif
+#endif // SYS_SERIAL_LOGGING
 				}
 				hw_timer_paused = FALSE;
 			}
 			return(SUCCESS);
 		}
 	}
-
+#ifdef SYS_SERIAL_LOGGING
 #if (SYS_LOG_LEVEL <= LOG_ERROR)
 	LOG_E("start_timer() ERR_NO_RESOURCES\n\r");
 #endif
+#endif // SYS_SERIAL_LOGGING
 	return(ERR_NO_RESOURCES);
 
 #elif defined(ES_LINUX)
@@ -357,24 +366,29 @@ result_t sw_timer_start(timer_id *timer, struct timer_req *request)
 	ret = timer_create(CLOCK_REALTIME, &action, timer);
 
 	if(ret) {
+#ifdef SYS_SERIAL_LOGGING
 #if (SYS_LOG_LEVEL <= LOG_ERROR)
 		LOG_E("Error can't create timer\n\r");
 #endif
+#endif // SYS_SERIAL_LOGGING
 		return(ERR_GENERAL_ERROR);
 	}
-	
+#ifdef SYS_SERIAL_LOGGING	
 #if (DEBUG_FILE && (SYS_LOG_LEVEL <= LOG_DEBUG))
 //	LOG_D("Setting time to %d Seconds %d nano Seonds\n\r", 
 #endif
+#endif // SYS_SERIAL_LOGGING
 	its.it_value.tv_sec = (ticks * SYS_SW_TIMER_TICK_ms) / 1000;
 	its.it_value.tv_nsec = (ticks * SYS_SW_TIMER_TICK_ms) % 1000 * 1000000;
 	its.it_interval.tv_sec = 0;
 	its.it_interval.tv_nsec = 0;
 
 	if (timer_settime(timer, 0, &its, NULL) == -1) {
+#ifdef SYS_SERIAL_LOGGING
 #if (SYS_LOG_LEVEL <= LOG_ERROR)
 		LOG_E("Error can't set time\n\r");
 #endif
+#endif // SYS_SERIAL_LOGGING
 		return(ERR_GENERAL_ERROR);
 	}
 
@@ -397,18 +411,22 @@ result_t sw_timer_cancel(timer_id timer)
 {
 #if defined(XC16) || defined(__XC8)
 	if ((timer == BAD_TIMER_ID) || (timer >= SYS_NUMBER_OF_SW_TIMERS)) {
+#ifdef SYS_SERIAL_LOGGING
 #if (SYS_LOG_LEVEL <= LOG_ERROR)
                 LOG_E("sw_timer_cancel() Bad timer identifier passed in!\n\r");
 #endif
+#endif // SYS_SERIAL_LOGGING
                 return(ERR_BAD_INPUT_PARAMETER);
 	} else if (timers[timer].active) {
                 timers[timer].active = FALSE;
                 timers[timer].expiry_count = 0;
                 timers[timer].function = (expiry_function) NULL;
         } else {
+#ifdef SYS_SERIAL_LOGGING
 #if (DEBUG_FILE == TRUE) && (SYS_LOG_LEVEL <= LOG_INFO)
                 LOG_I("sw_timer_cancel() timer not active!\n\r");
 #endif
+#endif // SYS_SERIAL_LOGGING
         }
         
 #elif defined(ES_LINUX)
@@ -420,9 +438,11 @@ result_t sw_timer_cancel(timer_id timer)
         its.it_interval.tv_nsec = 0;
 
         if (timer_settime(timer, 0, &its, NULL) == -1) {
+#ifdef SYS_SERIAL_LOGGING
 #if (SYS_LOG_LEVEL <= LOG_ERROR)
                 LOG_E("Error can't create timer\n\r");
 #endif
+#endif // SYS_SERIAL_LOGGING
                 return(ERR_GENERAL_ERROR);
         }
 #endif
@@ -436,10 +456,11 @@ result_t sw_timer_cancel(timer_id timer)
 result_t sw_timer_cancel_all(void)
 {
 	uint8_t    loop;
-
+#ifdef SYS_SERIAL_LOGGING
 #if (DEBUG_FILE && (SYS_LOG_LEVEL <= LOG_DEBUG))
 	LOG_D("sw_timer_cancel_all()\n\r");
 #endif
+#endif // SYS_SERIAL_LOGGING
 	for (loop = 0; loop < SYS_NUMBER_OF_SW_TIMERS; loop++) {
 		if (timers[loop].active) {
 			sw_timer_cancel(loop);
