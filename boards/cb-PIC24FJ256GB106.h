26bf767b97cff04404bfc6c708316883b5d44b72
John Whitmore <johnfwhitmore@gmail.com>
2017-11-21 16:21:54 +0000

    Added a dummy pin value if a function isn't used

--- boards/cb-PIC24FJ256GB106.h
+++ boards/cb-PIC24FJ256GB106.h
@@ -60,13 +60,13 @@
 //        #define SERIAL_LOGGING_TX        RPOR12bits.RP25R
 //        #define SERIAL_LOGGING_RX_PIN    RP20
         #define SERIAL_LOGGING_TX_PIN    RP25//    RPOR10bits.RP20R
-        #define SERIAL_LOGGING_RX_PIN    RP20
+        #define SERIAL_LOGGING_RX_PIN    NO_PIN // RP20
 #elif defined(SYS_SERIAL_PORT_GndRxTx)
 //        #define SERIAL_LOGGING_RX_DDR    TRISDbits.TRISD4
 //        #define SERIAL_LOGGING_TX_DDR    TRISDbits.TRISD5
 
         #define SERIAL_LOGGING_TX_PIN    RP20//    RPOR10bits.RP20R
-        #define SERIAL_LOGGING_RX_PIN    RP25
+        #define SERIAL_LOGGING_RX_PIN    NO_PIN  // RP25
 #endif
 
 /*
