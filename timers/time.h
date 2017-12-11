8834454b113671543b7e731775d37b9edcffccdf
John Whitmore <johnfwhitmore@gmail.com>
2017-12-11 20:45:56 +0000

    Correct delay.c as hw timer API has changed

--- timers/time.h
+++ timers/time.h
@@ -62,6 +62,7 @@ typedef uint8_t timer_id;
  * \brief Dummy timer identifier for a non existent timer.
  */
 #define BAD_TIMER_ID   0xff
+#define TIMER_INIT(timer) timer = BAD_TIMER_ID;
 
 #if defined(XC16) || defined(__XC8)
 /**
