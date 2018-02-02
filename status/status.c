7fb37baf6bbe018201e821812c486331f79407d1
John Whitmore <johnfwhitmore@gmail.com>
2018-02-02 21:18:02 +0000

    Added a system status type

---
+++ status/status.c
@@ -0,0 +1,23 @@
+/**
+ *
+ * libesoup/status/statuc.c
+ *
+ * File containing the function to initialise the libesoup library
+ *
+ * Copyright 2017 electronicSoup Limited
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the version 2 of the GNU Lesser General Public License
+ * as published by the Free Software Foundation
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ *******************************************************************************
+ *
+ */
---
+++ status/status.h
@@ -0,0 +1,52 @@
+/**
+ *
+ * libesoup/status/status.h
+ *
+ * Definitions for the status handler sub-system
+ *
+ * Copyright 2018 electronicSoup Limited
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the version 2 of the GNU Lesser General Public License
+ * as published by the Free Software Foundation
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ *******************************************************************************
+ *
+ */
+#ifndef _STATUS_H
+#define _STATUS_H
+
+typedef enum {
+#if defined(SYS_CAN_BUS)
+	can_bus_status,
+#endif
+#if defined(SYS_CAN_DCNCP)
+    can_bus_dcncp_status,
+#endif
+#if defined(ISO15765)
+    iso15765_status,
+#endif
+#if defined(ISO11783)
+    iso11783_status
+#endif
+} status_source_t;
+
+union ty_status {
+    struct {
+        status_source_t source;
+        uint8_t         status;
+    } sstruct;
+    uint16_t sword;
+};
+
+typedef void (*status_handler_t)(union ty_status);
+
+#endif // _STATUS_H
