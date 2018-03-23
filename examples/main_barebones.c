91ec3fbd6631a397ac34a69a1d6a9c75f2e59481
John Whitmore <johnfwhitmore@gmail.com>
2018-03-23 14:49:16 +0000

    Added file headers and comments to new project files

--- examples/main_barebones.c
+++ examples/main_barebones.c
@@ -1,5 +1,30 @@
-
-
+/**
+ *
+ * libesoup/examples/main_barebones.c
+ *
+ * An example main.c file for the bare bones minimum required to use the
+ * libesoup library of code.
+ * 
+ * The code is used in the example MPLAB-X project:
+ * libesoup/examples/projects/microchip/BareBones.X
+ *
+ * Copyright 2018 electronicSoup Limited
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the version 2 of the GNU Lesser General Public License
+ * as published by the Free Software Foundation
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ *******************************************************************************
+ *
+ */
 #include "libesoup_config.h"
 
 int main()
--- examples/projects/microchip/BareBones.X/libesoup_config.h
+++ examples/projects/microchip/BareBones.X/libesoup_config.h
@@ -1,4 +1,31 @@
-
+/**
+ *
+ * libesoup/examples/projects/microchip/BareBones.X/libesoup_config.h
+ *
+ * Example minimum configuration file for using the libesoup library of code.
+ * Simply specifies the desired clock speed and includes the board file for the
+ * target cinnamonBun board.
+ * 
+ * The code is used in the example MPLAB-X project:
+ * libesoup/examples/projects/microchip/BareBones.X
+ *
+ * Copyright 2018 electronicSoup Limited
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the version 2 of the GNU Lesser General Public License
+ * as published by the Free Software Foundation
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ *******************************************************************************
+ *
+ */
 #include <xc.h>
 
 //#define SYS_CLOCK_FREQ  8000000
