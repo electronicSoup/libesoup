49744c3314402a32f10cd001c9acf1607cc95eec
John Whitmore <johnfwhitmore@gmail.com>
2018-03-25 12:21:29 +0100

    added first pass of one wire census of network

--- comms/one_wire/one_wire.c
+++ comms/one_wire/one_wire.c
@@ -93,14 +93,14 @@ static struct one_wire_device device[SYS_ONE_WIRE_MAX_DEVICES];
 /*
  * Function Prototypes.
  */
-static result_t census(struct one_wire_bus *bus);
-static void     bus_change(uint8_t *port, uint8_t bit);
-static result_t reset_pulse(struct one_wire_bus *bus);
-static result_t write_byte(struct one_wire_bus *bus, uint8_t byte);
-static result_t write_one(struct one_wire_bus *bus);
-static result_t write_zero(struct one_wire_bus *bus);
-static result_t read_byte(struct one_wire_bus *bus);
-static result_t read_bit(struct one_wire_bus *bus);
+static result_t census(int16_t chan);
+static void     bus_change(uint16_t *port, uint8_t bit);
+static result_t reset_pulse(int16_t chan);
+static result_t write_byte(int16_t chan, uint8_t byte);
+static result_t write_one(int16_t chan);
+static result_t write_zero(int16_t chan);
+static result_t read_byte(int16_t chan);
+static result_t read_bit(int16_t chan);
 
 
 result_t one_wire_init()
@@ -113,15 +113,21 @@ result_t one_wire_init()
 	}
 
 	for(loop = 0; loop < SYS_ONE_WIRE_MAX_DEVICES; loop++) {
-		device[loop].channel = NO_CHANNEL;
+		device[loop].ow_channel = NO_CHANNEL;
 	}
 	return(0);
 }
 
-result_t one_wire_reserve(int16_t chan, enum pin_t pin)
+/*
+ * Create a OneWire bus on the given pin
+ * 
+ * Returns the created channel number
+ */
+result_t one_wire_reserve(enum pin_t pin)
 {
 	result_t rc;
 	uint8_t  loop;
+	int16_t  chan;
 #ifdef SERIAL_LOGGING	
 	LOG_D("one_wire_reserve()\n\r");
 #endif
@@ -135,20 +141,12 @@ result_t one_wire_reserve(int16_t chan, enum pin_t pin)
 	}
 
 	/*
-	 * If passed a channel number check it's available and valid
+	 * Find a free slot
 	 */
-	if(chan != OW_ANY_CHANNEL) {
-		if(chan < SYS_ONE_WIRE_MAX_BUS) return(-ERR_BAD_INPUT_PARAMETER);
-		if(bus[chan].active) return(-ERR_BUSY);		
-	} else {
-		/*
-	         * Find a free slot
-	         */
-		for(loop = 0; loop < SYS_ONE_WIRE_MAX_BUS; loop++) {
-			if(!bus[loop].active) {
-				chan = loop;
-				break;
-			}
+	for(loop = 0; loop < SYS_ONE_WIRE_MAX_BUS; loop++) {
+		if(!bus[loop].active) {
+			chan = loop;
+			break;
 		}
 	}
 	
@@ -160,56 +158,92 @@ result_t one_wire_reserve(int16_t chan, enum pin_t pin)
 		rc = gpio_set(pin, GPIO_MODE_DIGITAL_OUTPUT | GPIO_MODE_OPENDRAIN_OUTPUT, 1);
 		RC_CHECK
 
-		return(census(&bus[chan]));
+		rc = census(chan);
+		RC_CHECK
+		return(chan);
 	}
 	return(ERR_NO_RESOURCES);
 }
 
-static result_t census(struct one_wire_bus *bus)
+void store_bit(int8_t index, uint8_t bit, boolean value)
+{
+	if(value) {
+		if(bit < 8) {
+			device[index].family |= 0b1 << bit;
+		}
+	}	
+}
+
+static result_t census(int16_t chan)
 {
 	result_t  rc;
-	uint8_t   family_code = 0x00;
+	uint8_t   loop;
 	boolean   first_read;
 	boolean   second_read;
+	uint8_t   last_discrepency;
+	uint8_t   device_index;
 
 	delay(mSeconds, 100);	
 	
-	rc = reset_pulse(bus);
+	rc = reset_pulse(chan);
 	RC_CHECK
 	if(rc == 0) return(-ERR_NO_RESPONSE);
 
-	rc = write_byte(bus, SEARCH);
+	rc = write_byte(chan, SEARCH);
 //	rc = write_byte(bus, READ_ROM);
 //	LOG_D("write_byte returned %d\n\r", rc);
 	RC_CHECK
 
-	rc = read_bit(bus);
-	RC_CHECK
-	first_read = (boolean)(rc == 1);
-	rc = read_bit(bus);
-	RC_CHECK
-	second_read = (boolean)(rc == 1);
-	
-	if( (first_read == 0) && (second_read == 0) ) {
-		// Discrepancy at this bit
-	} else if( (first_read == 1) && (second_read == 1) ) {
-		// No devices, must be finished
-	} else if(first_read) {
-		// bit is one
-	} else {
-		// bit is zero
-	}
+	last_discrepency = 0x00;
+	device_index = 0;
 
-	rc = read_byte(bus);
-	RC_CHECK
-	family_code = rc;
+	while(last_discrepency != 0xff) {
+		last_discrepency = 0xff;  // Assume there will be no discrepency on the pass
+		device[device_index].family     = 0x00;
+		device[device_index].crc        = 0x00;
+		device[device_index].ow_channel = chan;
+		for(loop = 0; loop < 6; loop++) device[device_index].serial_number[loop] = 0x00;
+
+		for(loop = 0; loop < 64; loop++) {
+			rc = read_bit(chan);
+			RC_CHECK
+			first_read = (boolean)(rc == 1);
+
+			rc = read_bit(chan);
+			RC_CHECK
+			second_read = (boolean)(rc == 1);
+	
+			if( (first_read == 0) && (second_read == 0) ) {
+				// Discrepancy at this bit
+				last_discrepency = loop;
+				LOG_D("D\n\r");
+			} else if( (first_read == 1) && (second_read == 1) ) {
+				// No devices, must be finished
+				LOG_D("*\n\r");
+				break;
+			} else if(first_read) {
+				LOG_D("1\n\r");
+				store_bit(device_index, loop, first_read);
+				rc = write_one(chan);
+				RC_CHECK
+			} else {
+				LOG_D("0\n\r");
+				rc = write_zero(chan);
+				RC_CHECK
+			}
+		}
+		device_index++;
+	}
+	//	rc = read_byte(bus);
+	//	RC_CHECK
+	//	family_code = rc;
 #ifdef SYS_SERIAL_LOGGING
-	LOG_D("Family Code 0x%x\n\r", family_code);
+//	LOG_D("Family Code 0x%x\n\r", family_code);
 #endif
 	return(0);
 }
 
-static void bus_change(uint8_t *port, uint8_t bit)
+static void bus_change(uint16_t *port, uint8_t bit)
 {
 	bus_changes++;
 }
@@ -217,7 +251,7 @@ static void bus_change(uint8_t *port, uint8_t bit)
 /*
  * Synchronous Function it will spin
  */
-static result_t reset_pulse(struct one_wire_bus *bus)
+static result_t reset_pulse(int16_t chan)
 {
         result_t rc;
         
@@ -226,26 +260,26 @@ static result_t reset_pulse(struct one_wire_bus *bus)
 	 * so request change notification on the bus I/O Pin
 	 */
 	bus_changes = 0;
-	rc = change_notifier_register(bus->pin, bus_change);
+	rc = change_notifier_register(bus[chan].pin, bus_change);
 	RC_CHECK
 	
         delay(mSeconds, 100);
-	rc = gpio_set(bus->pin, GPIO_MODE_DIGITAL_OUTPUT | GPIO_MODE_OPENDRAIN_OUTPUT, 0);
+	rc = gpio_set(bus[chan].pin, GPIO_MODE_DIGITAL_OUTPUT | GPIO_MODE_OPENDRAIN_OUTPUT, 0);
 	RC_CHECK
         delay(uSeconds, 500);
-	rc = gpio_set(bus->pin, GPIO_MODE_DIGITAL_OUTPUT | GPIO_MODE_OPENDRAIN_OUTPUT, 1);
-	rc = gpio_set(bus->pin, GPIO_MODE_DIGITAL_INPUT | GPIO_MODE_OPENDRAIN_OUTPUT, 1);
+	rc = gpio_set(bus[chan].pin, GPIO_MODE_DIGITAL_OUTPUT | GPIO_MODE_OPENDRAIN_OUTPUT, 1);
+	rc = gpio_set(bus[chan].pin, GPIO_MODE_DIGITAL_INPUT | GPIO_MODE_OPENDRAIN_OUTPUT, 1);
 	RC_CHECK
 		
 	// Give the slaves on the bus time to respond
 	delay(uSeconds, 500);
-	rc = change_notifier_deregister(bus->pin);
+	rc = change_notifier_deregister(bus[chan].pin);
 	RC_CHECK
 
 	return(bus_changes);
 }
 
-static result_t write_byte(struct one_wire_bus *bus, uint8_t byte)
+static result_t write_byte(int16_t chan, uint8_t byte)
 {
         uint8_t  bit = 0;
         uint8_t  i;
@@ -261,21 +295,21 @@ static result_t write_byte(struct one_wire_bus *bus, uint8_t byte)
                  * Check is the bit zero or one
                  */
                 if(byte & bit) {
-			rc = write_one(bus);
+			rc = write_one(chan);
 			RC_CHECK
 		} else {
-			rc = write_zero(bus);
+			rc = write_zero(chan);
 			RC_CHECK
 		}
 	}
         return(0);
 }
 
-result_t write_one(struct one_wire_bus *bus)
+result_t write_one(int16_t chan)
 {
 	result_t  rc;
 
-	rc = gpio_set(bus->pin, GPIO_MODE_DIGITAL_OUTPUT | GPIO_MODE_OPENDRAIN_OUTPUT, 0);
+	rc = gpio_set(bus[chan].pin, GPIO_MODE_DIGITAL_OUTPUT | GPIO_MODE_OPENDRAIN_OUTPUT, 0);
 	RC_CHECK
 	Nop();
 	Nop();
@@ -285,7 +319,7 @@ result_t write_one(struct one_wire_bus *bus)
 	Nop();
 	Nop();
 	Nop();
-	rc = gpio_set(bus->pin, GPIO_MODE_DIGITAL_OUTPUT | GPIO_MODE_OPENDRAIN_OUTPUT, 1);
+	rc = gpio_set(bus[chan].pin, GPIO_MODE_DIGITAL_OUTPUT | GPIO_MODE_OPENDRAIN_OUTPUT, 1);
 	RC_CHECK
 		
 	// Wait for the end of the write slot
@@ -294,15 +328,15 @@ result_t write_one(struct one_wire_bus *bus)
 	return(0);
 }
 
-result_t write_zero(struct one_wire_bus *bus)
+result_t write_zero(int16_t chan)
 {
 	result_t  rc;
 	
-	rc = gpio_set(bus->pin, GPIO_MODE_DIGITAL_OUTPUT | GPIO_MODE_OPENDRAIN_OUTPUT, 0);
+	rc = gpio_set(bus[chan].pin, GPIO_MODE_DIGITAL_OUTPUT | GPIO_MODE_OPENDRAIN_OUTPUT, 0);
 	RC_CHECK
         rc = delay(uSeconds, 100);
 	RC_CHECK
-	rc = gpio_set(bus->pin, GPIO_MODE_DIGITAL_OUTPUT | GPIO_MODE_OPENDRAIN_OUTPUT, 1);
+	rc = gpio_set(bus[chan].pin, GPIO_MODE_DIGITAL_OUTPUT | GPIO_MODE_OPENDRAIN_OUTPUT, 1);
 	RC_CHECK
         rc = delay(uSeconds, 20);
 	RC_CHECK
@@ -310,14 +344,14 @@ result_t write_zero(struct one_wire_bus *bus)
 	return(0);
 }
 
-static result_t read_byte(struct one_wire_bus *bus)
+static result_t read_byte(int16_t chan)
 {
 	result_t  rc;
 	uint8_t   loop;
 	uint8_t   byte = 0x00;
 
 	for(loop = 0; loop < 8; loop++) {
-		rc = read_bit(bus);
+		rc = read_bit(chan);
 		RC_CHECK
 			
 		byte = byte >> 1;
@@ -330,12 +364,12 @@ static result_t read_byte(struct one_wire_bus *bus)
 	return(byte);
 }
 
-static result_t read_bit(struct one_wire_bus *bus)
+static result_t read_bit(int16_t chan)
 {
 	int16_t   bit;
 	result_t  rc;
 	
-	rc = gpio_set(bus->pin, GPIO_MODE_DIGITAL_OUTPUT | GPIO_MODE_OPENDRAIN_OUTPUT, 0);
+	rc = gpio_set(bus[chan].pin, GPIO_MODE_DIGITAL_OUTPUT | GPIO_MODE_OPENDRAIN_OUTPUT, 0);
 	RC_CHECK
 	Nop();
 	Nop();
@@ -353,15 +387,15 @@ static result_t read_bit(struct one_wire_bus *bus)
 	Nop();
 	Nop();
 	Nop();
-	rc = gpio_set(bus->pin, GPIO_MODE_DIGITAL_OUTPUT | GPIO_MODE_OPENDRAIN_OUTPUT, 1);
+	rc = gpio_set(bus[chan].pin, GPIO_MODE_DIGITAL_OUTPUT | GPIO_MODE_OPENDRAIN_OUTPUT, 1);
 	RC_CHECK
-	rc = gpio_set(bus->pin, GPIO_MODE_DIGITAL_INPUT | GPIO_MODE_OPENDRAIN_INPUT, 1);
+	rc = gpio_set(bus[chan].pin, GPIO_MODE_DIGITAL_INPUT | GPIO_MODE_OPENDRAIN_INPUT, 1);
 	RC_CHECK
 	Nop();
 	Nop();
 	Nop();
 	Nop();
-	rc = gpio_get(bus->pin);
+	rc = gpio_get(bus[chan].pin);
 	RC_CHECK
 	bit = rc;
 //	LOG_D("read bit %d\n\r", rc);
