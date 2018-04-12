8b415764b0f81a6174d78b7e01f862d100d5b6b0
John Whitmore <johnfwhitmore@gmail.com>
2018-04-12 17:53:17 +0100

    Simple indentation

--- processors/dsPIC33EP256MU806.c
+++ processors/dsPIC33EP256MU806.c
@@ -41,20 +41,22 @@ static const char *TAG = "dsPIC33";
  *
  * file:///opt/microchip/xc16/v1.26/docs/config_docs/33EP256MU806.html
  */
-#pragma config GWRP = OFF
-#pragma config GSS = OFF
-#pragma config GSSK = OFF
+#pragma config GWRP    = OFF
+#pragma config GSS     = OFF
+#pragma config GSSK    = OFF
 //#pragma config FNOSC = FRC
-#pragma config FNOSC = PRI   // Primary oscillator
+#pragma config FNOSC   = PRI   // Primary oscillator
 //#pragma config FNOSC = SOSC  // Secondary oscillator
-#pragma config IESO = OFF
-#pragma config POSCMD = HS
+#pragma config IESO    = OFF
+#pragma config POSCMD  = HS
 #pragma config IOL1WAY = OFF
-#pragma config FCKSM = CSECMD
-#pragma config FWDTEN = OFF
-#pragma config WINDIS   = OFF              // Watchdog Timer Window (Standard Watchdog Timer enabled,(Windowed-mode is disabled))
-#pragma config BOREN = OFF
-#pragma config JTAGEN = OFF
+#pragma config FCKSM   = CSECMD
+#pragma config FWDTEN  = OFF    // Enable the Watch Dog Timer
+#pragma config WDTPRE  = PR128  // 1:128  
+#pragma config WDTPOST = PS128  // 1:128
+#pragma config WINDIS  = ON     // Watchdog Timer Window disabled
+#pragma config BOREN   = OFF
+#pragma config JTAGEN  = OFF
 
 static void clock_init(void);
 
