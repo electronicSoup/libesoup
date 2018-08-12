84b154b546021a6af9fbf297d3a1d1de0103b2a5
John Whitmore <johnfwhitmore@gmail.com>
2018-08-12 18:28:54 +0100

    Added templates for the PWM API functions.
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>

---
+++ gpio/pwm.c
@@ -0,0 +1,36 @@
+/**
+ * @file libesoup/gpio/pwm.c
+ *
+ * @author John Whitmore
+ * 
+ * @brief API implementation for PWM functions
+ * 
+ * Copyright 2018 electronicSoup Limited
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the version 2 of the GNU Lesser General Public License
+ * as published by the Free Software Foundation
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+result_t pwm_config(enum gpio pin, frequengy, duty)
+{
+	return(0);
+}
+
+result_t pwm_on(enum gpio pin)
+{
+	return(0);
+}
+
+result_t pwm_off(enum gpio pin)
+{
+	return(0);
+}
