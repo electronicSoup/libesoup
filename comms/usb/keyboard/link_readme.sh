7af09dcd7d6917fcd42a7264af96a69db529e1d8
John Whitmore <johnfwhitmore@gmail.com>
2020-08-29 08:09:55 +0100

    Added another file to the link script, missed one.

--- comms/usb/keyboard/link_readme.sh
+++ comms/usb/keyboard/link_readme.sh
@@ -14,3 +14,8 @@ ln -s /opt/microchip/mla/v2018_11_26/framework/usb/src/usb_device.c     ./mcp_us
 ln -s /opt/microchip/mla/v2018_11_26/framework/usb/src/usb_device_hid.c ./mcp_usb_device_hid.c
 ln -s /opt/microchip/mla/v2018_11_26/framework/usb/src/usb_hal_16bit.c  ./mcp_usb_hal_16bit.c
 
+#
+# This is a header file so we have to keep the same name. Alternatively
+# we could add this directory to the Compiler's Include search path,
+# but that's already getting a bit budsy.
+ln -s /opt/microchip/mla/v2018_11_26/framework/usb/src/usb_device_local.h
